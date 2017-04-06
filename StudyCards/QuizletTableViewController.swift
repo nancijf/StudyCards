4527712d8f87de7879fc0d1aa3090fce23b9aa1d
Nanci Frank <me@nancifrank.com>
2017-04-06 10:48:50 -0400

    Fixed issue with searchBar not resizing when device rotates on QuizletVC

--- StudyCards/QuizletTableViewController.swift
+++ StudyCards/QuizletTableViewController.swift
@@ -15,11 +15,18 @@ class QuizletTableViewController: UITableViewController, UISearchBarDelegate, UI
     var qlCardData = [CardStruct]()
     let qzSearchController = UISearchController(searchResultsController: nil)
     var timer: Timer? = nil
+    var searchWidthAnchor: NSLayoutConstraint?
     
     let cellIdentifier = "qlCellIdentifier"
     
+    lazy var searchBar: UISearchBar = {
+        let searchBarWidth = self.view.frame.width
+        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: searchBarWidth, height: 44.0))
+        return searchBar
+    }()
+    
     lazy var addCancelButton: UIBarButtonItem = {
-        let addCancelButton = UIBarButtonItem(title: "Dismiss Search", style: .plain, target: self, action: #selector(cancelSearch) )
+        let addCancelButton = UIBarButtonItem(title: "Hide Search", style: .plain, target: self, action: #selector(cancelSearch) )
         addCancelButton.isEnabled = true
         
         return addCancelButton
@@ -28,21 +35,23 @@ class QuizletTableViewController: UITableViewController, UISearchBarDelegate, UI
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        qzSearchController.searchResultsUpdater = self
-        qzSearchController.dimsBackgroundDuringPresentation = false
-        qzSearchController.searchBar.placeholder = "Search Quizlet"
-        qzSearchController.delegate = self
-        qzSearchController.searchBar.delegate = self
-        qzSearchController.hidesNavigationBarDuringPresentation = false
-        qzSearchController.searchBar.showsCancelButton = false
-        
+//        qzSearchController.searchResultsUpdater = self
+//        qzSearchController.dimsBackgroundDuringPresentation = false
+//        qzSearchController.searchBar.placeholder = "Search Quizlet"
+//        qzSearchController.delegate = self
+//        qzSearchController.searchBar.delegate = self
+        createViews()
+
+        definesPresentationContext = false
+
+        searchBar.delegate = self
+        searchBar.placeholder = "Search Quizlet..."
         let viewWidth = self.view.frame.width
         let navBar = UINavigationBar(frame: CGRect(x: 0, y: 0, width: viewWidth, height: 44))
         let navItem = UINavigationItem()
         navItem.rightBarButtonItem = addCancelButton
         navBar.pushItem(navItem, animated: false)
-        qzSearchController.searchBar.inputAccessoryView = navBar
-        
+        searchBar.inputAccessoryView = navBar
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -50,38 +59,43 @@ class QuizletTableViewController: UITableViewController, UISearchBarDelegate, UI
         
         self.tabBarController?.navigationItem.leftBarButtonItem = nil
         self.tabBarController?.navigationItem.rightBarButtonItem = nil
-        qzSearchController.view.translatesAutoresizingMaskIntoConstraints = false
-        view.addSubview(qzSearchController.view)
-        qzSearchController.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
-        qzSearchController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
-        qzSearchController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
-        qzSearchController.view.heightAnchor.constraint(equalToConstant: 44).isActive = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
 
-        if let tabBarHeight = self.tabBarController?.tabBar.frame.size.height {
-            self.tableView.contentInset = UIEdgeInsetsMake(44, 0, tabBarHeight, 0)
-        }
+//        if let tabBarHeight = self.tabBarController?.tabBar.frame.size.height {
+//            self.tableView.contentInset = UIEdgeInsetsMake(44, 0, tabBarHeight, 0)
+//        }
 
-        qzSearchController.isActive = true
+//        qzSearchController.isActive = true
+        searchBar.becomeFirstResponder()
     }
     
     override func viewWillDisappear(_ animated: Bool) {
         super.viewWillDisappear(animated)
-        qzSearchController.isActive = false
+        searchBar.resignFirstResponder()
+//        qzSearchController.isActive = false
     }
     
     override func viewDidLayoutSubviews() {
-        var searchBarFrame = qzSearchController.searchBar.frame
-        searchBarFrame.size.width = view.frame.size.width
-        qzSearchController.searchBar.frame = searchBarFrame
+        updateViews()
+//        qzSearchController.searchBar.showsCancelButton = false
+//        var searchBarFrame = qzSearchController.searchBar.frame
+//        searchBarFrame.size.width = view.frame.size.width
+//        view.setNeedsUpdateConstraints()
+//        view.updateConstraintsIfNeeded()
+//        qzSearchController.searchBar.frame = searchBarFrame
+//        view.layoutIfNeeded()
+//        self.updateViewConstraints()
+//        view.updateConstraintsIfNeeded()
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
+    
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let indexPath = self.tableView.indexPathForSelectedRow {
@@ -93,14 +107,59 @@ class QuizletTableViewController: UITableViewController, UISearchBarDelegate, UI
         }
     }
     
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        super.viewWillTransition(to: size, with: coordinator)
+        view.setNeedsLayout()
+        view.layoutIfNeeded()
+    }
+        
     func cancelSearch() {
-        qzSearchController.isActive = false
+        searchBar.resignFirstResponder()
+//        qzSearchController.isActive = false
     }
     
-    func didPresentSearchController(_ searchController: UISearchController) {
-        if let tabBarHeight = self.tabBarController?.tabBar.frame.size.height {
-            self.tableView.contentInset = UIEdgeInsetsMake(44, 0, tabBarHeight, 0)
-        }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        timer?.invalidate()
+        timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(searchQZ), userInfo: nil, repeats: false)
+    }
+
+    
+//    func didPresentSearchController(_ searchController: UISearchController) {
+//
+//        if let tabBarHeight = self.tabBarController?.tabBar.frame.size.height {
+//            self.tableView.contentInset = UIEdgeInsetsMake(44, 0, tabBarHeight, 0)
+//        }
+//    }
+    
+    func createViews() {
+//        qzSearchController.view.translatesAutoresizingMaskIntoConstraints = false
+//        view.addSubview(qzSearchController.view)
+//        qzSearchController.view.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+//        qzSearchController.view.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+//        qzSearchController.view.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+//        qzSearchController.view.heightAnchor.constraint(equalToConstant: 44).isActive = true
+//        searchWidthAnchor = qzSearchController.view.widthAnchor.constraint(equalToConstant: view.frame.size.width)
+//        searchWidthAnchor?.isActive = true
+        searchBar.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(searchBar)
+        searchBar.topAnchor.constraint(equalTo: self.view.topAnchor).isActive = true
+        searchBar.leftAnchor.constraint(equalTo: self.view.leftAnchor).isActive = true
+        searchBar.rightAnchor.constraint(equalTo: self.view.rightAnchor).isActive = true
+        searchBar.heightAnchor.constraint(equalToConstant: 44).isActive = true
+        searchWidthAnchor = qzSearchController.view.widthAnchor.constraint(equalToConstant: view.frame.size.width)
+        searchWidthAnchor?.isActive = true
+
+    }
+    
+    func updateViews() {
+        searchBar.showsCancelButton = false
+        searchBar.frame.size.width = view.frame.size.width
+        searchWidthAnchor?.constant = view.frame.size.width
+        searchWidthAnchor?.isActive = true
+        view.setNeedsUpdateConstraints()
+        view.updateConstraintsIfNeeded()
+        
     }
     
     func didDismissSearchController(_ searchController: UISearchController) {
@@ -115,13 +174,15 @@ class QuizletTableViewController: UITableViewController, UISearchBarDelegate, UI
         self.dismiss(animated: false, completion: nil)
     }
     
+
+    
     func updateSearchResults(for searchController: UISearchController) {
         timer?.invalidate()
         timer = Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(searchQZ), userInfo: nil, repeats: false)
     }
 
     func searchQZ() {
-        if let searchText = qzSearchController.searchBar.text, searchText.characters.count > 1 {
+        if let searchText = searchBar.text, searchText.characters.count > 1 {
             if let escapedText = searchText.addingPercentEncoding(withAllowedCharacters: CharacterSet.urlQueryAllowed) {
                 quizletController.searchQuizlet(escapedText, onSuccess: { [weak self] (quizletData) in
                     DispatchQueue.main.async(execute: {
